6eb040dd9ec276df2f978be9a9821be9c4781c62
farahmuhammed99 <91473118+farahmuhammed99@users.noreply.github.com>
2024-01-30 20:59:34 +0200

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    url: "http://automationpractice.multiformis.com/index.php",
+  
+    elements: {
+      contactusButton: {
+        selector: "#contact-link",
+      },
+    },
+  };
