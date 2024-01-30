7bc19a8353932305e8728c6735fd35923e8a0f25
farahmuhammed99 <91473118+farahmuhammed99@users.noreply.github.com>
2024-01-30 21:10:52 +0200

    SDET

--- pages/index/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    url: "http://automationpractice.multiformis.com/index.php",
-  
-    elements: {
-      contactusButton: {
-        selector: "#contact-link",
-      },
-    },
-  };
